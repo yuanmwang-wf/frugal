a58929919fb73bae63b5ef49095c4f463f5bc3ea
Charlie Strawn <charlie.strawn@workiva.com>
2017-09-12 15:24:29 -0500

    Convert frugal binary to use glide instead of godep

--- scripts/skynet/skynet_setup.sh
+++ scripts/skynet/skynet_setup.sh
@@ -5,7 +5,6 @@ set -exo pipefail
 mkdir -p /go/src/github.com/Workiva/
 
 # Move godeps to gopath for both library and binary
-cp -r /testing/Godeps/_workspace/* $GOPATH/
 cp -r /testing/lib/go/Godeps/_workspace/* $GOPATH/
 
 
@@ -19,8 +18,7 @@ if [ -z "${IN_SKYNET_CLI+yes}" ]; then
     mkdir $GOPATH/src/github.com/Workiva/frugal/test/integration/log
     chmod 755 ${GOPATH}/bin/frugal
 else
-    cd $GOPATH/src/github.com/Workiva/frugal
-    godep go install
+    cd $GOPATH/src/github.com/Workiva/frugal && go install
 fi
 
 
--- scripts/smithy.sh
+++ scripts/smithy.sh
@@ -8,7 +8,6 @@ python $SMITHY_ROOT/scripts/smithy/verify_pr_target.py
 mkdir -p $SMITHY_ROOT/test_results/
 
 # Move godeps to gopath
-cp -r $FRUGAL_HOME/Godeps/_workspace/* $GOPATH/
 cp -r $FRUGAL_HOME/lib/go/Godeps/_workspace/* $GOPATH/
 
 # Run each language build and tests in parallel

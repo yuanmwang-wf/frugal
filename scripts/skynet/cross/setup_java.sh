b3222143116e7f53a848e57baa2772b80c3d0b63
Yuan.Wang <yuan.m.wang@workiva.com>
2018-12-19 14:17:35 -0600

    debugging

--- scripts/skynet/cross/setup_java.sh
+++ scripts/skynet/cross/setup_java.sh
@@ -13,7 +13,8 @@ else
 fi
 
 cd ${FRUGAL_HOME}/test/integration/java/frugal-integration-test
-mvn clean install:install-file -Dfile=frugal.jar -U -q
+mvn -v
+mvn clean install:install-file -Dfile=frugal.jar -U -q -X
 
 # Compile java tests
 mvn clean compile assembly:single -U -q

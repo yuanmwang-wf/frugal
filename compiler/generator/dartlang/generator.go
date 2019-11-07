c28394c5436a4505f1367cab50716af25a4622a7
evanweible-wf <evan.weible@workiva.com>
2019-11-07 16:38:21 -0700

    Bump thrift and w_common versions as well.

--- compiler/generator/dartlang/generator.go
+++ compiler/generator/dartlang/generator.go
@@ -196,7 +196,7 @@ func (g *Generator) addToPubspec(dir string) error {
 		"logging":        "^0.11.2",
 		"thrift": dep{
 			Hosted:  hostedDep{Name: "thrift", URL: "https://pub.workiva.org"},
-			Version: "^0.0.7",
+			Version: "^0.0.9",
 		},
 	}
 

8a7540b024c4cedc9717a31d8b7eaaf8af2aba08
olesiathoms-wk <olesia.thoms@workiva.com>
2019-11-17 18:46:55 -0700

    update gen files

--- test/expected/dart/variety/f_foo_transitive_deps_service.dart
+++ test/expected/dart/variety/f_foo_transitive_deps_service.dart
@@ -9,43 +9,42 @@ import 'package:logging/logging.dart' as logging;
 import 'package:thrift/thrift.dart' as thrift;
 import 'package:frugal/frugal.dart' as frugal;
 
-import 'package:vendor_namespace/vendor_namespace.dart' as t_vendor_namespace;
-import 'package:excepts/excepts.dart' as t_excepts;
-import 'package:include_vendor/include_vendor.dart' as t_include_vendor;
+import 'package:intermediate_include/intermediate_include.dart' as t_intermediate_include;
+import 'package:variety/variety.dart' as t_variety;
 
 
-abstract class FMyService extends t_vendor_namespace.FVendoredBase {
-  Future<t_vendor_namespace.Item> getItem(frugal.FContext ctx);
+abstract class FFooTransitiveDeps extends t_intermediate_include.FIntermediateFoo {
+  Future ping(frugal.FContext ctx);
 }
 
-class FMyServiceClient extends t_vendor_namespace.FVendoredBaseClient implements FMyService {
-  static final logging.Logger _frugalLog = logging.Logger('MyService');
+class FFooTransitiveDepsClient extends t_intermediate_include.FIntermediateFooClient implements FFooTransitiveDeps {
+  static final logging.Logger _frugalLog = logging.Logger('FooTransitiveDeps');
   Map<String, frugal.FMethod> _methods;
 
-  FMyServiceClient(frugal.FServiceProvider provider, [List<frugal.Middleware> middleware])
+  FFooTransitiveDepsClient(frugal.FServiceProvider provider, [List<frugal.Middleware> middleware])
       : super(provider, middleware) {
     _transport = provider.transport;
     _protocolFactory = provider.protocolFactory;
     var combined = middleware ?? [];
     combined.addAll(provider.middleware);
     this._methods = {};
-    this._methods['getItem'] = frugal.FMethod(this._getItem, 'MyService', 'getItem', combined);
+    this._methods['ping'] = frugal.FMethod(this._ping, 'FooTransitiveDeps', 'ping', combined);
   }
 
   frugal.FTransport _transport;
   frugal.FProtocolFactory _protocolFactory;
 
   @override
-  Future<t_vendor_namespace.Item> getItem(frugal.FContext ctx) {
-    return this._methods['getItem']([ctx]).then((value) => value as t_vendor_namespace.Item);
+  Future ping(frugal.FContext ctx) {
+    return this._methods['ping']([ctx]);
   }
 
-  Future<t_vendor_namespace.Item> _getItem(frugal.FContext ctx) async {
+  Future _ping(frugal.FContext ctx) async {
     var memoryBuffer = frugal.TMemoryOutputBuffer(_transport.requestSizeLimit);
     var oprot = _protocolFactory.getProtocol(memoryBuffer);
     oprot.writeRequestHeader(ctx);
-    oprot.writeMessageBegin(thrift.TMessage('getItem', thrift.TMessageType.CALL, 0));
-    getItem_args args = getItem_args();
+    oprot.writeMessageBegin(thrift.TMessage('ping', thrift.TMessageType.CALL, 0));
+    ping_args args = ping_args();
     args.write(oprot);
     oprot.writeMessageEnd();
     var response = await _transport.request(ctx, memoryBuffer.writeBytes);
@@ -63,29 +62,19 @@ class FMyServiceClient extends t_vendor_namespace.FVendoredBaseClient implements
       throw error;
     }
 
-    getItem_result result = getItem_result();
+    ping_result result = ping_result();
     result.read(iprot);
     iprot.readMessageEnd();
-    if (result.isSetSuccess()) {
-      return result.success;
-    }
-
-    if (result.d != null) {
-      throw result.d;
-    }
-    throw thrift.TApplicationError(
-      frugal.FrugalTApplicationErrorType.MISSING_RESULT, 'getItem failed: unknown result'
-    );
   }
 }
 
 // ignore: camel_case_types
-class getItem_args implements thrift.TBase {
-  static final thrift.TStruct _STRUCT_DESC = thrift.TStruct('getItem_args');
+class ping_args implements thrift.TBase {
+  static final thrift.TStruct _STRUCT_DESC = thrift.TStruct('ping_args');
 
 
 
-  getItem_args() {}
+  ping_args() {}
 
   @override
   getFieldValue(int fieldID) {
@@ -142,7 +131,7 @@ class getItem_args implements thrift.TBase {
 
   @override
   String toString() {
-    StringBuffer ret = StringBuffer('getItem_args(');
+    StringBuffer ret = StringBuffer('ping_args(');
 
     ret.write(')');
 
@@ -151,7 +140,7 @@ class getItem_args implements thrift.TBase {
 
   @override
   bool operator ==(Object o) {
-    return o is getItem_args;
+    return o is ping_args;
   }
 
   @override
@@ -160,8 +149,8 @@ class getItem_args implements thrift.TBase {
     return value;
   }
 
-  getItem_args clone() {
-    return getItem_args();
+  ping_args clone() {
+    return ping_args();
   }
 
   validate() {
@@ -170,51 +159,16 @@ class getItem_args implements thrift.TBase {
   }
 }
 // ignore: camel_case_types
-class getItem_result implements thrift.TBase {
-  static final thrift.TStruct _STRUCT_DESC = thrift.TStruct('getItem_result');
-  static final thrift.TField _SUCCESS_FIELD_DESC = thrift.TField('success', thrift.TType.STRUCT, 0);
-  static final thrift.TField _D_FIELD_DESC = thrift.TField('d', thrift.TType.STRUCT, 1);
-
-  t_vendor_namespace.Item _success;
-  static const int SUCCESS = 0;
-  t_excepts.InvalidData _d;
-  static const int D = 1;
+class ping_result implements thrift.TBase {
+  static final thrift.TStruct _STRUCT_DESC = thrift.TStruct('ping_result');
 
 
-  getItem_result() {
-  }
 
-  t_vendor_namespace.Item get success => this._success;
-
-  set success(t_vendor_namespace.Item success) {
-    this._success = success;
-  }
-
-  bool isSetSuccess() => this.success != null;
-
-  unsetSuccess() {
-    this.success = null;
-  }
-
-  t_excepts.InvalidData get d => this._d;
-
-  set d(t_excepts.InvalidData d) {
-    this._d = d;
-  }
-
-  bool isSetD() => this.d != null;
-
-  unsetD() {
-    this.d = null;
-  }
+  ping_result() {}
 
   @override
   getFieldValue(int fieldID) {
     switch (fieldID) {
-      case SUCCESS:
-        return this.success;
-      case D:
-        return this.d;
       default:
         throw ArgumentError("Field $fieldID doesn't exist!");
     }
@@ -223,22 +177,6 @@ class getItem_result implements thrift.TBase {
   @override
   setFieldValue(int fieldID, Object value) {
     switch (fieldID) {
-      case SUCCESS:
-        if (value == null) {
-          unsetSuccess();
-        } else {
-          this.success = value as t_vendor_namespace.Item;
-        }
-        break;
-
-      case D:
-        if (value == null) {
-          unsetD();
-        } else {
-          this.d = value as t_excepts.InvalidData;
-        }
-        break;
-
       default:
         throw ArgumentError("Field $fieldID doesn't exist!");
     }
@@ -248,10 +186,6 @@ class getItem_result implements thrift.TBase {
   @override
   bool isSet(int fieldID) {
     switch (fieldID) {
-      case SUCCESS:
-        return isSetSuccess();
-      case D:
-        return isSetD();
       default:
         throw ArgumentError("Field $fieldID doesn't exist!");
     }
@@ -264,22 +198,6 @@ class getItem_result implements thrift.TBase {
         field.type != thrift.TType.STOP;
         field = iprot.readFieldBegin()) {
       switch (field.id) {
-        case SUCCESS:
-          if (field.type == thrift.TType.STRUCT) {
-            this.success = t_vendor_namespace.Item();
-            success.read(iprot);
-          } else {
-            thrift.TProtocolUtil.skip(iprot, field.type);
-          }
-          break;
-        case D:
-          if (field.type == thrift.TType.STRUCT) {
-            this.d = t_excepts.InvalidData();
-            d.read(iprot);
-          } else {
-            thrift.TProtocolUtil.skip(iprot, field.type);
-          }
-          break;
         default:
           thrift.TProtocolUtil.skip(iprot, field.type);
           break;
@@ -297,42 +215,13 @@ class getItem_result implements thrift.TBase {
     validate();
 
     oprot.writeStructBegin(_STRUCT_DESC);
-    if (isSetSuccess() && this.success != null) {
-      oprot.writeFieldBegin(_SUCCESS_FIELD_DESC);
-      this.success.write(oprot);
-      oprot.writeFieldEnd();
-    }
-    if (isSetD() && this.d != null) {
-      oprot.writeFieldBegin(_D_FIELD_DESC);
-      this.d.write(oprot);
-      oprot.writeFieldEnd();
-    }
     oprot.writeFieldStop();
     oprot.writeStructEnd();
   }
 
   @override
   String toString() {
-    StringBuffer ret = StringBuffer('getItem_result(');
-
-    if (isSetSuccess()) {
-      ret.write('success:');
-      if (this.success == null) {
-        ret.write('null');
-      } else {
-        ret.write(this.success);
-      }
-    }
-
-    if (isSetD()) {
-      ret.write(', ');
-      ret.write('d:');
-      if (this.d == null) {
-        ret.write('null');
-      } else {
-        ret.write(this.d);
-      }
-    }
+    StringBuffer ret = StringBuffer('ping_result(');
 
     ret.write(')');
 
@@ -341,28 +230,17 @@ class getItem_result implements thrift.TBase {
 
   @override
   bool operator ==(Object o) {
-    if (o is getItem_result) {
-      return this.success == o.success &&
-        this.d == o.d;
-    }
-    return false;
+    return o is ping_result;
   }
 
   @override
   int get hashCode {
     var value = 17;
-    value = (value * 31) ^ this.success.hashCode;
-    value = (value * 31) ^ this.d.hashCode;
     return value;
   }
 
-  getItem_result clone({
-    t_vendor_namespace.Item success = null,
-    t_excepts.InvalidData d = null,
-  }) {
-    return getItem_result()
-      ..success = success ?? this.success
-      ..d = d ?? this.d;
+  ping_result clone() {
+    return ping_result();
   }
 
   validate() {
